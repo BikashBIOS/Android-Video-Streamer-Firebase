29a7eb083cf6ccab564fd7966255358eb77104f6
Bikash Ranjan Ojha <bikashojha101@gmail.com>
2020-09-15 15:47:46 +0530

    lacking frontend part

---
+++ app/src/main/java/com/example/videostreamer/Member.java
@@ -0,0 +1,42 @@
+package com.example.videostreamer;
+
+public class Member {
+    private String name;
+    private String Videourl;
+    private String search;
+
+
+    public Member() {
+    }
+
+    public Member(String name, String videourl, String search) {
+        this.name = name;
+        Videourl = videourl;
+        this.search = search;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVideourl() {
+        return Videourl;
+    }
+
+    public void setVideourl(String videourl) {
+        Videourl = videourl;
+    }
+
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
+    }
+
+}
